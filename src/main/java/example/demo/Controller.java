58a651c73ffb76e0b60748667c45df92e9833566
dani <danii>
2020-02-21 08:52:33 -0800

    jj

---
+++ src/main/java/example/demo/Controller.java
@@ -0,0 +1,17 @@
+package example.demo;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+@RestController
+@EnableAutoConfiguration
+public class Controller {
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(Controller.class, args);
+    }
+}
+
